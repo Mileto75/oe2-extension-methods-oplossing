0c431cf177e3576feb7dfa03cc3c37059182cf86
mileto1975 <mileto.di.marco@howest.be>
2019-06-19 18:59:19 +0200

    Fixed string interpolatie

--- ExtensionMethodsOef/Program.cs
+++ ExtensionMethodsOef/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExtensionMethodsOef.Models;
+using System;
+using System.Collections.Generic;
 
 namespace ExtensionMethodsOef
 {
@@ -6,7 +8,28 @@ namespace ExtensionMethodsOef
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //make a string list
+            List<string> LeveranciersInfo = new List<string>();
+            IEnumerable<Leveranciers> LeveranciersLijst = Leveranciers.getLeveranciers();
+            foreach(Leveranciers l in LeveranciersLijst)
+            {
+                LeveranciersInfo.Add($"Naam:{l.Naam}\nAdres:{l.Adres.Straat} {l.Adres.Nummer}, {l.Adres.PostCode} {l.Adres.Gemeente}" +
+                    $"\nHeeftSchuld:{l.HeeftSchuld}\nTegoed:{l.Tegoed}");
+            }
+
+            foreach(string l in LeveranciersInfo)
+            {
+                Console.WriteLine(l);
+            }
+            //PrintLijst(LeveranciersInfo);
+            Console.ReadKey();
+        }
+        public static void PrintLijst(List<string> lijst)
+        {
+            foreach(string l in lijst)
+            {
+                Console.WriteLine(l);
+            }
         }
     }
 }
